c807bd62e004c80949759513c6022140d4c1ba80
arthur laine <arthur.laine99@gmail.com>
2021-01-26 14:07:34 +0100

    read csv infos and added correct names for each column, added -g bonus working with -c for the color of each histogram

---
+++ data_infos.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import csv
+import pandas as pd
+import sys
+from copy import deepcopy
+import utils
+
+if __name__ == "__main__":
+    args = utils.arg_parse()
+    reader = utils.get_csv_infos()
+    if args.graph:
+        tmp_reader = deepcopy(reader)
+        c = 'black'
+        for k in reader:
+            if "ID" not in k: #Pass some useless histographs to show like ID
+                if args.color:
+                    c = utils.parse_color_hist(args.color)
+                tmp_reader[str(k)].hist(color=c, alpha=0.5)
+                plt.title(str(k))
+                plt.xlabel("Value")
+                plt.ylabel("Number of elements")
+                plt.show()
+            else:
+                pass
