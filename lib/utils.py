a713b92a6973dff67ddf70aeee359be0df502d2b
arthur laine <arthur.laine99@gmail.com>
2021-02-08 15:35:45 +0100

    fresh start, should be the one this time

--- lib/utils.py
+++ lib/utils.py
@@ -2,12 +2,49 @@ import pandas as pd
 import argparse
 import sys
 
+def get_min_max_scale(data):
+    min_ = 0
+    max_ = 0
+    for i in range(len(data)):
+        for j in range(len(data.iloc[i])):
+            if data.iloc[i][j] > max_:
+                max_ = data.iloc[i][j]
+            elif data.iloc[i][j] < min_:
+                min_ = data.iloc[i][j]
+    return [min_, max_]
+
+def separate_data(data):
+    train = data.iloc[0:400]
+    test = data.iloc[400:]
+    return train, test
+
 def arg_parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('-g', '--graph', action='store_true', help='Print historgrams')
     parser.add_argument('-c', '--color', action='store', help='histogram color')
+    parser.add_argument('-p', '--plot', action='store_true', help='print plot')
     options = parser.parse_args()
     return options
+
+def append_new_array(base_array, new_array):
+    new_array_casted = []
+    for i in range(len(new_array)):
+        new_array_casted.append(float(new_array[i]))
+    base_array.append(new_array_casted)
+    return base_array
+
+def create_multi_dim_array(nb_i_dim, size_j_dim, value):
+    new_array = []
+    for i in range(nb_i_dim):
+        new_array.append([])
+        for j in range(size_j_dim):
+            new_array[i].append(value)
+    return new_array
+
+def create_empty_array(nb_elems, value):
+    x = value
+    array = [x for i in range(nb_elems)]
+    return array
 
 def get_csv_infos():
     lst_names = ['ID', 'Diagnosis', 'Radius', 'Texture', 'Perimeter', 'Area', 'Smoothness', 'Compactness', \
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import pandas as pd
 import sys
 from copy import deepcopy
 #from lib import utils, maths, neural_network as nn
-from lib import utils, maths, neural as nn
+#from lib import utils, maths, neural as nn
+#from lib import utils, maths, n as nn
+from lib import utils, test as nn
 
 if __name__ == "__main__":
     args = utils.arg_parse()
@@ -28,49 +30,16 @@ if __name__ == "__main__":
     desired_outputs = []
     for i in range(len(reader['Diagnosis'])):
         values[i] = 0 if reader['Diagnosis'][i] == 'B' else 1
-        desired_outputs.append(values[i])
+        desired_outputs.append([values[i]])
     reader['Diagnosis'] = values
-    desired_outputs = reader['Diagnosis']
+    #desired_outputs = reader['Diagnosis']
+    if args.plot:
+        nn.scatter(reader)
     del reader['Diagnosis']
-    test = nn.NeuralNetwork(reader, desired_outputs, 2, [20, 20], 2, 'train')
-    test.fit(reader)
-    #test.__str__()
-    #test = nn.NeuralNetwork(reader, 2, 10, 2, desired_outputs)
-    #test = nn.NeuralNetwork(reader, 2, 10, 2)
-    #test.run()
-    #test.__str__()
-    
-    #test_w = [0.3, 1.2, -0.4, 1.1]
-    #print("weighted sum for {} = {}".format(test_w, nn.weighted_sum(test_w, 0.75)))
-    #----------------------Test math functions-------------------------------#
-
-    """print("Max from numpy.amax() :")
-    print(math.max(pd.DataFrame(test).to_numpy()))
-    print("Max from lib.math :")
-    print(math.max(pd.DataFrame(test).to_numpy(), True), '\n')
-
-    print("Min from numpy.amin() :")
-    print(math.min(pd.DataFrame(test).to_numpy()))
-    print("Min from lib.math : ")
-    print(math.min(pd.DataFrame(test).to_numpy(), True), '\n')
-
-    print("Mean from numpy.mean() : ")
-    print(math.mean(pd.DataFrame(test).to_numpy()))
-    print("Mean from lib.math : ")
-    print(math.mean(pd.DataFrame(test).to_numpy(), True), '\n')
-
-    print("Standard Error from numpy.std() : ")
-    print(math.standard_error(pd.DataFrame(test).to_numpy()))
-    print("Standard Error from lib.math : ")
-    print(math.standard_error(pd.DataFrame(test).to_numpy(), True), '\n')
-
-    print("Standard deviation from numpy.std() / (np.size(array)**0.5) : ")
-    print(math.standard_deviation(pd.DataFrame(test).to_numpy()))
-    print("Standard deviation from lib.math : ")
-    print(math.standard_deviation(pd.DataFrame(test).to_numpy(), True), '\n')
-
-    print("Variance from numpy.std() / numpy.mean() : ")
-    print(math.variance(pd.DataFrame(test).to_numpy()))
-    print("Variance from lib.math : ")
-    print(math.variance(pd.DataFrame(test).to_numpy(), True), '\n')"""
-    #------------------------------------------------------------------------#
+    train, test = utils.separate_data(reader)
+    t = nn.NeuralNetwork(train, desired_outputs, 2, 20)
+    #t.__str__()
+    #nn.plot_training_test_samples(train, test)
+    #scale = utils.get_min_max_scale(train)
+    #nn.main_network([30, 20, 20, 2], train, test, desired_outputs, scale)
+    #del reader['Diagnosis']
